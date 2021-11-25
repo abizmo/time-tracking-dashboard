b62f5221cfda5b9e49f6f08ec35bcbe4d059269e
abizmo <abizmo@gmail.com>
2021-11-25 15:21:43 +0000

    Remove @import Rubik

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -1,8 +1,6 @@
 import { css, createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap');
-
   * {
     box-sizing: border-box;
   }
